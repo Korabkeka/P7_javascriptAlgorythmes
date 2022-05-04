94a492c73ff8fcb83dbbff1cc11b245fe67e6a6b
Korab <87022376+Korabkeka@users.noreply.github.com>
2022-05-04 11:24:39 +0200

    get all keywords

--- index.js
+++ index.js
@@ -1,45 +1,100 @@
 // algorythme using for, while, do while...
 
+// =========================================================================== //
+
 const searchBar = document.getElementById('search-bar__input');
 
 const _ingredients = [];
 const _appareils = [];
 const _ustensils = [];
 
+class KeyWord{
+    constructor(keyWord){
+        this.keyWord = keyWord.toLowerCase();
+    }
+}
+
+class Ingredient extends KeyWord{
+    constructor(keyWord){
+        super(keyWord)
+    }
+}
+
+class Appliance extends KeyWord{
+    constructor(keyWord){
+        super(keyWord)
+    }
+}
+
+class Ustensil extends KeyWord{
+    constructor(keyWord){
+        super(keyWord)
+    }
+}
+
+
 function checkIfExist(item, array){
     if(array.length === 0){
         array.push(item);
     }else{
+        let exists = false;
         for (let i = 0; i < array.length; i++){
-            if(array[i]===item){
-                console.log(`${item} is already in array`)
-                break;
-            }else{
-                if(i === array.length - 1 && array[i] !== item){
-                    array.push(item);
-                }
-            } 
+            if(array[i].keyWord===item.keyWord){
+                exists = true;
+            }
         }
-    }
-
-    
+        if(!exists){
+            array.push(item);
+        }
+    }   
 }
 
 for (let i = 0; i < recipes.length; i++){
 
     for(let j = 0; j < recipes[i].ingredients.length; j++){
-        let tested = recipes[i].ingredients[j].ingredient;
+        let tested = new Ingredient(recipes[i].ingredients[j].ingredient);
         checkIfExist(tested, _ingredients);
     }
-    if(_appareils.indexOf(recipes[i].appliance) === -1){
-        _appareils.push(recipes[i].appliance);
-    }
+    checkIfExist(new Appliance(recipes[i].appliance), _appareils);
+    
     for(let k = 0; k < recipes[i].ustensils.length; k++){
-        let tested = recipes[i].ustensils[k];
-        if(_ustensils.indexOf(tested)===-1){
-            _ustensils.push(tested);
-        }
+        let tested = new Ustensil(recipes[i].ustensils[k]);
+        checkIfExist(tested, _ustensils);
     }
 }
 
-console.log(_ingredients);
+const allKeyWords = _ingredients.concat(_appareils, _ustensils);
+
+console.log(allKeyWords);
+
+searchBar.addEventListener('input', function(){
+
+    if(this.value.length > 2){
+
+        let a = filterRecipes(this.value);
+        console.log(a);
+    }
+
+});
+
+function filterRecipes(input){
+    const result = [];
+
+    for(let i = 0; i < recipes.length; i++){
+        if(recipes[i].name.toLocaleLowerCase().includes(input)){
+            result.push(recipes[i]);
+        }else if(recipes[i].description.toLocaleLowerCase().includes(input)){
+            result.push(recipes[i]);
+        }else{
+            for (let j = 0; j < recipes[i].ingredients.length; j++){
+                if(recipes[i].ingredients[j].ingredient.toLocaleLowerCase().includes(input)){
+                    result.push(recipes[i]);
+                }
+            }
+        }
+
+    }
+
+    return result;
+
+}
